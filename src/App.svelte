31965b466c27d9095af2f96f227bdbe16ac4199d
Armando Couto <coutoarmando@gmail.com>
2022-01-18 20:50:40 -0300

    Commit com a POC inicial.

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Nested from './Nested.svelte';
+	import Click from './Click.svelte';
+	import DomEvents from './DomEvents.svelte';
+</script>
+
+<p>These styles...</p>
+<Nested/>
+<hr/>
+<Click/>
+<hr/>
+<DomEvents/>
+
+<style>
+	p {
+		color: purple;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+</style>
